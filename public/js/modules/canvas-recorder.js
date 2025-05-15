33e75ef2fb29ee570ecf2b2987be675b3655fb2e
Grayson Earle <graysonearle@gmail.com>
2025-05-14 21:39:51 -0400

    increased size of props

--- public/js/modules/canvas-recorder.js
+++
@@ -1,8 +0,0 @@
-import { logToConsole } from './logger.js';
-
-// mainMediaRecorder, mainRecordedChunks, isMainCanvasRecording, appConfig, fetchAppConfig, and initializeCanvasRecorder are removed.
-// If other canvas recording functionalities exist in this file, they would remain.
-// For now, this file will be nearly empty if initializeCanvasRecorder was the only major function.
-
-// Example: Keeping other potential (unrelated) exports if they existed.
-// export function someOtherCanvasFunction() { /* ... */ } 
